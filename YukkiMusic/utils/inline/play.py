26647089a821127d0ad36db67d703b86782c8159
MS-DZULQURNAIN <121719789+MS-DZULQURNAIN@users.noreply.github.com>
2023-07-28 21:02:29 +0700

    Update play.py

--- YukkiMusic/utils/inline/play.py
+++ YukkiMusic/utils/inline/play.py
@@ -17,7 +17,7 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
                 text=_["PL_B_3"],  switch_inline_query_current_chat=""
             ),
             InlineKeyboardButton(
-                text=_["S_B_9"], url=f"https://t.me/MSPR0JECT",
+                text=_["S_B_9"], url=f"https://t.me/DezetStore",
             ),
         ],
         [
@@ -65,7 +65,7 @@ def stream_markup(_, videoid):
         [
              
             InlineKeyboardButton(
-                text=_["S_B_9"], url=f"https://t.me/MSPR0JECT",
+                text=_["S_B_9"], url=f"https://t.me/DezetStore",
             ),
 
 
